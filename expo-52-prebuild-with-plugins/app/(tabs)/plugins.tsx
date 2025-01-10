7742bb1a83f33567ceec0b51eef4ffe53fe6d474
Krzysztof Magiera <krzys@swmansion.com>
2025-01-10 12:48:34 +0100

    Add expo dev plugins to expo-52 and rename to expo-52-prebuild-with-plugins

---
+++ expo-52-prebuild-with-plugins/app/(tabs)/plugins.tsx
@@ -0,0 +1,90 @@
+import { useScheme } from '@/shared/Colors';
+import { Button, View } from 'react-native';
+
+import devToolsEnhancer from 'redux-devtools-expo-dev-plugin';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+  useQueryClient,
+} from '@tanstack/react-query';
+import { configureStore } from '@reduxjs/toolkit';
+import { useReactQueryDevTools } from '@dev-plugins/react-query';
+import { useSelector, useDispatch, Provider } from 'react-redux';
+
+const queryClient = new QueryClient();
+
+const initialState = {
+  count: 0,
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, count: state.count + 1 };
+    case 'DECREMENT':
+      return { ...state, count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: false,
+  enhancers: (getDefaultEnhancers) =>
+    getDefaultEnhancers().concat(devToolsEnhancer()),
+});
+
+function ReduxCounter() {
+  const count = useSelector((state: { count: number }) => state.count);
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({ type: 'INCREMENT' });
+  };
+
+  return <Button onPress={increment} title={`Redux counter: ${count}`} />;
+}
+
+const fetchCounter = async () => {
+  return 0; // Initial counter value
+};
+
+const ReactQueryCounter = () => {
+  const queryClient = useQueryClient();
+
+  const { data: count = 0 } = useQuery({
+    queryKey: ['counter'],
+    queryFn: fetchCounter,
+  });
+
+  const increment = () => {
+    queryClient.setQueryData(['counter'], (oldCount: number) => oldCount + 1);
+  };
+
+  return <Button onPress={increment} title={`React Query counter: ${count}`} />;
+};
+
+export default function TabTwoScreen() {
+  const { colors } = useScheme();
+
+  useReactQueryDevTools(queryClient);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <View
+          style={{
+            backgroundColor: colors.background,
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <ReduxCounter />
+          <ReactQueryCounter />
+        </View>
+      </Provider>
+    </QueryClientProvider>
+  );
+}
